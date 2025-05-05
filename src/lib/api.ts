b83574f4677350b2c44d302ea30359402141239e
Desmond Egya <egyadesmond@gmail.com>
2025-05-05 22:29:50 +0100

    Fix API base URL by removing duplicate slash

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nex-be-ohft.onrender.com//",
+  baseURL: "https://nex-be-ohft.onrender.com/",
 });
 
 export default api;
