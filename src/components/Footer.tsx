494d598656666bfa529d57d66b25f97b6204ecba
Desmond Egya <egyadesmond@gmail.com>
2025-05-18 23:19:27 +0100

    Build terms of service page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+import { Instagram, X } from "./Icons";
+import Image from "next/image";
+
+const Footer = () => (
+  <footer className="bg-[#6C35A7] text-white py-12 px-6 md:px-20 lg:px-28  items-center md:items-start flex flex-col gap-10">
+    <div className="flex flex-col md:flex-row justify-between gap-10 w-full items-center md:items-start">
+      <div className="flex flex-col md:flex-row gap-8 lg:gap-16 font-bold text-[20px] md:text-[22px] lg:text-[24px] leading-[49px] text-center md:text-left">
+        <Link href="#">Home</Link>
+        <Link href="#">Terms & Conditions</Link>
+        <Link href="#">Privacy Policy</Link>
+      </div>
+      <div className="flex gap-6 ">
+        <div className="bg-white w-[56px] h-[56px] flex items-center justify-center rounded-2xl">
+          <Instagram />
+        </div>
+        <div className="bg-white w-[56px] h-[56px] flex items-center justify-center rounded-2xl">
+          <X />
+        </div>
+      </div>
+    </div>
+    <div className="flex flex-col md:flex-row justify-between items-center md:items-end gap-6 w-full">
+      <Image
+        src="/nex-white.svg"
+        alt="Nex Bookings logo"
+        width={135}
+        height={38}
+        className="w-[112px] h-[32px] md:w-[135px] md:h-[38px]"
+      />
+      <p className="text-[24px] font-bold">Info@nexbookings.com</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/components/vendor/profile/AccountSettings.tsx
+++ src/components/vendor/profile/AccountSettings.tsx
@@ -1,27 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AccountSettings() {
   return (
     <div className="w-full">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
-        <div className="flex bg-[#F6F6F6] text-[#6C35A7] font-bold text-[17px] justify-between p-6 rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300">
-          Privacy Policy
-          <Image
-            src="/nex-right.svg"
-            alt="Nex Bookings logo"
-            width={24}
-            height={24}
-          />
-        </div>
-        <div className="flex bg-[#F6F6F6] text-[#6C35A7] font-bold text-[17px] justify-between p-6 rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300">
-          Terms of Service
-          <Image
-            src="/nex-right.svg"
-            alt="Green right arrow"
-            width={24}
-            height={24}
-          />
-        </div>
+        <Link href="/privacy-policy">
+          <div className="flex bg-[#F6F6F6] text-[#6C35A7] font-bold text-[17px] justify-between p-6 rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300">
+            Privacy Policy
+            <Image
+              src="/nex-right.svg"
+              alt="Nex Bookings logo"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+        <Link href="/terms-of-service">
+          <div className="flex bg-[#F6F6F6] text-[#6C35A7] font-bold text-[17px] justify-between p-6 rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300">
+            Terms of Service
+            <Image
+              src="/nex-right.svg"
+              alt="Green right arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
         <div className="flex bg-[#F6F6F6] text-[#F62E2E] font-bold text-[17px] justify-between p-6 rounded-lg cursor-pointer hover:bg-gray-200 transition-colors duration-300">
           Delete Account
           <Image
